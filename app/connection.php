458ca361099cda526b4cf42c823beb271bfed44a
ilCONDORA <125373443+ilCONDORA@users.noreply.github.com>
2023-10-25 11:32:16 +0200

    crd

--- app/connection.php
+++ app/connection.php
@@ -1,25 +1,10 @@
 <?php
-//Simone
-/* $servername = "localhost";
-$database = "feedback4u";
-$username = "root";
-$password = "111022";
-$port = 3307; 
-*/
-//Condor
-/* $servername = "localhost";
-$database = "feedback4u";
-$username = "root";
-$password = "263732";
-$port = 3307;
- */
 //credenziali docker || NON TOCCARE!!!
 $servername = "feedback4u-db-1";
 $database = "feedback4u";
 $username = "fb4uAdmin";
 $password = "fb4uAdminfb4uAdminx2";
 $port = 3306;
-
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database, $port);
